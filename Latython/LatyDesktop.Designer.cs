e6c1537671525cd1186e34e728010697ad85ac7c
Leo <leonardo.dani2003@gmail.com>
2025-04-04 23:47:31 -0600

    Upload base proyect

---
+++ Latython/LatyDesktop.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Latython
+{
+    partial class LatyDesktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MenuStrip menuStrip1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LatyDesktop));
+            toolStripMenuItemCopiar = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem4 = new ToolStripMenuItem();
+            toolStripMenuItem5 = new ToolStripMenuItem();
+            cToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem6 = new ToolStripMenuItem();
+            avisos = new ToolStripMenuItem();
+            menuStrip2 = new MenuStrip();
+            arToolStripMenuItem = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            menuDeArchivoToolStripMenuItem = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            guardarComoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem1 = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            carga = new ToolStripProgressBar();
+            menuStrip1 = new MenuStrip();
+            menuStrip1.SuspendLayout();
+            menuStrip2.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.AutoSize = false;
+            menuStrip1.Dock = DockStyle.Bottom;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItemCopiar, toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4, toolStripMenuItem5, cToolStripMenuItem, toolStripMenuItem6, avisos });
+            menuStrip1.Location = new Point(0, 419);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RenderMode = ToolStripRenderMode.Professional;
+            menuStrip1.Size = new Size(800, 31);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // toolStripMenuItemCopiar
+            // 
+            toolStripMenuItemCopiar.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            toolStripMenuItemCopiar.Image = (Image)resources.GetObject("toolStripMenuItemCopiar.Image");
+            toolStripMenuItemCopiar.Name = "toolStripMenuItemCopiar";
+            toolStripMenuItemCopiar.Size = new Size(28, 27);
+            //toolStripMenuItemCopiar.Click += toolStripMenuItem1_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Image = (Image)resources.GetObject("toolStripMenuItem2.Image");
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(28, 27);
+            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Image = (Image)resources.GetObject("toolStripMenuItem3.Image");
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(28, 27);
+            toolStripMenuItem3.Click += toolStripMenuItem3_Click;
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.Image = (Image)resources.GetObject("toolStripMenuItem4.Image");
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new Size(28, 27);
+            toolStripMenuItem4.Click += toolStripMenuItem4_Click;
+            // 
+            // toolStripMenuItem5
+            // 
+            toolStripMenuItem5.Image = (Image)resources.GetObject("toolStripMenuItem5.Image");
+            toolStripMenuItem5.Name = "toolStripMenuItem5";
+            toolStripMenuItem5.Size = new Size(28, 27);
+            // 
+            // cToolStripMenuItem
+            // 
+            cToolStripMenuItem.Image = Properties.Resources.console;
+            cToolStripMenuItem.Name = "cToolStripMenuItem";
+            cToolStripMenuItem.Size = new Size(28, 27);
+            cToolStripMenuItem.Click += cToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem6
+            // 
+            toolStripMenuItem6.Image = Properties.Resources.salir;
+            toolStripMenuItem6.Name = "toolStripMenuItem6";
+            toolStripMenuItem6.Size = new Size(28, 27);
+            toolStripMenuItem6.Click += toolStripMenuItem6_Click;
+            // 
+            // avisos
+            // 
+            avisos.Name = "avisos";
+            avisos.Size = new Size(28, 27);
+            avisos.Text = "...";
+            // 
+            // menuStrip2
+            // 
+            menuStrip2.Items.AddRange(new ToolStripItem[] { arToolStripMenuItem, menuDeArchivoToolStripMenuItem, salirToolStripMenuItem1 });
+            menuStrip2.Location = new Point(0, 0);
+            menuStrip2.Name = "menuStrip2";
+            menuStrip2.Size = new Size(800, 24);
+            menuStrip2.TabIndex = 1;
+            menuStrip2.Text = "menuStrip2";
+            // 
+            // arToolStripMenuItem
+            // 
+            arToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { acercaDeToolStripMenuItem });
+            arToolStripMenuItem.Image = Properties.Resources.creditos;
+            arToolStripMenuItem.Name = "arToolStripMenuItem";
+            arToolStripMenuItem.Size = new Size(79, 20);
+            arToolStripMenuItem.Text = "Creditos";
+            arToolStripMenuItem.Click += arToolStripMenuItem_Click;
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.Image = Properties.Resources.creditos1;
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(126, 22);
+            acercaDeToolStripMenuItem.Text = "Acerca de";
+            acercaDeToolStripMenuItem.Click += acercaDeToolStripMenuItem_Click;
+            // 
+            // menuDeArchivoToolStripMenuItem
+            // 
+            menuDeArchivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nuevoToolStripMenuItem, abrirToolStripMenuItem, guardarToolStripMenuItem, guardarComoToolStripMenuItem, salirToolStripMenuItem });
+            menuDeArchivoToolStripMenuItem.Image = Properties.Resources.menu;
+            menuDeArchivoToolStripMenuItem.Name = "menuDeArchivoToolStripMenuItem";
+            menuDeArchivoToolStripMenuItem.Size = new Size(126, 20);
+            menuDeArchivoToolStripMenuItem.Text = "Menu de Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Image = Properties.Resources.document;
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(150, 22);
+            nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Image = Properties.Resources.folder;
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(150, 22);
+            abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.Image = Properties.Resources.disquete;
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(150, 22);
+            guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            guardarComoToolStripMenuItem.Image = Properties.Resources.disquete;
+            guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            guardarComoToolStripMenuItem.Size = new Size(150, 22);
+            guardarComoToolStripMenuItem.Text = "Guardar como";
+            guardarComoToolStripMenuItem.Click += guardarComoToolStripMenuItem_Click;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Image = Properties.Resources.salir;
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(150, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // salirToolStripMenuItem1
+            // 
+            salirToolStripMenuItem1.Image = Properties.Resources.salir;
+            salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            salirToolStripMenuItem1.Size = new Size(57, 20);
+            salirToolStripMenuItem1.Text = "Salir";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.LightSlateGray;
+            richTextBox1.Location = new Point(21, 27);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(755, 367);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, carga });
+            statusStrip1.Location = new Point(0, 397);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.DoubleClickEnabled = true;
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(0, 17);
+            toolStripStatusLabel1.Click += toolStripStatusLabel1_Click;
+            // 
+            // carga
+            // 
+            carga.Name = "carga";
+            carga.Size = new Size(100, 16);
+            carga.Step = 100;
+            carga.Value = 10;
+            // 
+            // LatyDesktop
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            Controls.Add(menuStrip2);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "LatyDesktop";
+            Text = "Latython";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            menuStrip2.ResumeLayout(false);
+            menuStrip2.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem arToolStripMenuItem;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
+        private ToolStripMenuItem menuDeArchivoToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItemCopiar;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private RichTextBox richTextBox1;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem toolStripMenuItem4;
+        private ToolStripMenuItem toolStripMenuItem5;
+        private ToolStripMenuItem cToolStripMenuItem;
+        private ToolStripMenuItem avisos;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar carga;
+        private ToolStripMenuItem toolStripMenuItem6;
+        private ToolStripMenuItem salirToolStripMenuItem1;
+    }
+}
